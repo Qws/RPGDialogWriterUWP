959ce002e8ae3af2c7ce001bbc2be1a629b47ed5
deno <deno@hotmail.nl>
2018-10-15 21:13:10 +0200

    Not working, experimental ViewModels

---
+++ RPGDialogWriterUWP/ViewModel/InteractableObjectsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.Storage;
+
+namespace RPGDialogWriterUWP.ViewModel
+{
+    class InteractableObjectsViewModel
+    {
+        private Model.StoryChapterModel storyChapterModel;
+        private Model.MapModel mapModel;
+        private List<Model.InteractableObjectModel> interactableObjectModels = new List<Model.InteractableObjectModel>();
+
+        public InteractableObjectsViewModel()
+        {
+            
+        }
+
+        private void FillDataWithJSON()
+        {
+            StorageFile file;
+        }
+    }
+}
---
+++ RPGDialogWriterUWP/ViewModel/MapStoryViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+using Windows.Data.Json;
+
+namespace RPGDialogWriterUWP.ViewModel
+{
+    class MapStoryViewModel : INotifyPropertyChanged
+    {
+        public Model.StoryChapterModel StoryChapterModel { get; set; }
+        public string Author { get; set; }
+
+        private Model.MapModel mapModel;
+        private Model.StoryChapterModel storyChapterModel;
+        private string author;
+        private DateTime datetime;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MapStoryViewModel()
+        {
+            
+        }
+
+        private async void CreateMapStoryJson()
+        {
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile sampleFile = await storageFolder.CreateFileAsync(mapModel.Name + "_" + storyChapterModel.Title + ".json", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+ 
+        }
+
+        private async Task CreateJSONFileAsync()
+        {
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile sampleFile = await storageFolder.CreateFileAsync( mapModel.Name + "_" +storyChapterModel.Title + ".json", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+            
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
