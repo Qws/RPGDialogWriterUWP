2e14140e164012e2db83570b9fb55aa27fa56296
Deno Suresan <deno@hotmail.nl>
2018-10-17 21:34:55 +0200

    Json to Model is working abit
    There is a known bug at line 103:
    string branchdata = branch.Value.GetString().ToString();

--- RPGDialogWriterUWP/ViewModel/LoadJSONViewModel.cs
+++ RPGDialogWriterUWP/ViewModel/LoadJSONViewModel.cs
@@ -69,11 +69,96 @@ namespace RPGDialogWriterUWP.ViewModel
         private async Task<Model.MapStoryModel> JSONtoModels(Windows.Storage.StorageFile file)
         {
             var dialog = new MessageDialog("");
-            if (file == null)
+            if (file != null)
+            {
+                string data = await Windows.Storage.FileIO.ReadTextAsync(file);
+                JsonObject jsonObj = new JsonObject();
+                jsonObj = JsonObject.Parse(data);
+
+                MapStory.StoryModel.Title = jsonObj.GetNamedString("title");
+                MapStory.StoryModel.Description = jsonObj.GetNamedString("description");
+                MapStory.StoryModel.Author = jsonObj.GetNamedString("author");
+
+                //JsonObject, to access Json Api for Interactable Object.
+                JsonObject interactableObjects = new JsonObject();
+
+                //Model, which will be put into the MapStory Model.
+                Model.InteractableObjectModel interactableObjectModel = new Model.InteractableObjectModel();
+                interactableObjects = jsonObj.GetNamedObject("interactableobjects");
+                //Multiple Interactable Objects usually. Has to dismantle and save them all into objects of the major Model: MapStory Model.
+                foreach (var obj in interactableObjects)
+                {
+                    //Interactable Object (characters, enemies, bosses, friends, talking chair, etc)
+                    dialog = new MessageDialog("obj key: " + obj.Key + " obj.value: "+obj.Value);
+                    await dialog.ShowAsync();
+                    interactableObjectModel.Name = obj.Key;
+                    JsonObject jsonInteracatbleObject = new JsonObject();
+                    jsonInteracatbleObject = JsonObject.Parse(obj.Value.ToString());
+                    // Each interactable Objects has multiple branches, options, states, feelings etc...
+                    foreach(var branch in jsonInteracatbleObject)
+                    {
+                        //Branch has a name.
+                        Model.BranchModel branchModel = new Model.BranchModel();
+                        branchModel.Name = branch.Key;
+                        string branchdata = branch.Value.GetString().ToString();
+                        JsonObject jsonBrancheInteraction = JsonObject.Parse(branchdata);
+
+                        //BranchModel added into the InteractableObjectModel. This process will repeatedly for the other branches.
+                        interactableObjectModel.Branches.Add(branchModel);
+                        //var dialogJson is not really a true JsonObject, it has only 2 properties (Key and Value).
+                        //Those values have to be converted to Strings from a so called: "KeyValuePair".
+                        foreach (var dialogJson in jsonBrancheInteraction)
+                        {
+                            JsonObject dialogJsonObject = JsonObject.Parse(dialogJson.Value.ToString());
+
+                            if(dialogJsonObject.ContainsKey("question"))
+                            {
+                                Model.QuestionModel questionModel = new Model.QuestionModel();
+                                questionModel.Name = dialogJsonObject.GetNamedValue("name").ToString();
+                                questionModel.Question = dialogJsonObject.GetNamedValue("question").ToString();
+                                questionModel.Emote = dialogJsonObject.GetNamedValue("emote").ToString();
+                                foreach (var choiceJson in dialogJsonObject.GetNamedObject("choices"))
+                                {
+                                    JsonObject jsonObjectChoice = JsonObject.Parse(choiceJson.Value.ToString());
+                                    Model.ChoiceModel choiceModel = new Model.ChoiceModel();
+                                    choiceModel.Name = jsonObjectChoice.GetNamedValue("name").ToString();
+                                    choiceModel.Target = jsonObjectChoice.GetNamedValue("target").ToString();
+                                    choiceModel.Description = jsonObjectChoice.GetNamedValue("description").ToString();
+                                    if(jsonObjectChoice.ContainsKey("branch"))
+                                    {
+                                        choiceModel.Branch = jsonObjectChoice.GetNamedValue("branch").ToString();
+                                    }
+                                }
+                            }
+
+                            if (int.TryParse(dialogJson.Key, out int dialognumber))
+                            {
+                                
+                            }
+
+                            else
+                            {
+
+                            }
+
+                        }
+                        //has chain of messages that the player has to go through.
+                        //continue here ->
+                        //extra info: go through another foreach. This time for the real dialogs.
+                    }
+                }
+            }
+
+            
+
+            else
             {
                 dialog = new MessageDialog("Error, file is null!");
                 await dialog.ShowAsync();
+
             }
+
+            
 
             if (MapStory != null)
             {
