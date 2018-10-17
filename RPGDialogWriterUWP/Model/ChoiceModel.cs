2bb3b9b481eac2dabebe59d06687cab4a31aa001
Deno Suresan <deno@hotmail.nl>
2018-10-17 21:32:25 +0200

    grammer fix branch (from branche)

--- RPGDialogWriterUWP/Model/ChoiceModel.cs
+++ RPGDialogWriterUWP/Model/ChoiceModel.cs
@@ -11,6 +11,6 @@ namespace RPGDialogWriterUWP.Model
         public string Name { get; set; }
         public string Description { get; set; }
         public string Target { get; set; }
-        public string Branche { get; set; }
+        public string Branch { get; set; }
     }
 }
