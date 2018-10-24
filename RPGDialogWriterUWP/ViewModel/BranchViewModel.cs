3ecd9ebc89fa460393e1ba45a8b4167fd06abe60
Deno Suresan <deno@hotmail.nl>
2018-10-24 21:37:59 +0200

    Added Branch View and VM. Not accessible in GUI.

---
+++ RPGDialogWriterUWP/ViewModel/BranchViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGDialogWriterUWP.ViewModel
+{
+    class BranchViewModel : ViewModel.BaseViewModel
+    {
+        private string title;
+        public string Title
+        {
+            get
+            {
+                title = "Branch Editing: "+CurrentBranch.Name;
+                return title;
+            }
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Model.Branch currentBranch;
+        public Model.Branch CurrentBranch
+        {
+            get { return currentBranch; }
+            set
+            {
+                currentBranch = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Model.Message> messageList;
+        public ObservableCollection<Model.Message> MessageList
+        {
+            get {return this.messageList; }
+            set
+            {
+                this.messageList = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public BranchViewModel(Model.MapStory pMapStory, Model.Branch pBranch)
+        {
+            this.SelectedMapStory = pMapStory;
+            this.CurrentBranch = pBranch;
+            var message = new Model.Message();
+            foreach (var m in currentBranch.Messages)
+            {
+                if(m.GetType() == typeof(Model.Message))
+                {
+                    message = m as Model.Message;
+                    MessageList.Add(message);
+                }
+
+                else if(m.GetType() == typeof(Model.Question))
+                {
+                    message = m as Model.Question;
+                    MessageList.Add(message);
+                }
+                else
+                {
+                    var dialog = new Windows.UI.Popups.MessageDialog("Error, unsupported Message type is tried to be added into the MessageList.");
+                }
+            }
+        }
+    }
+}
